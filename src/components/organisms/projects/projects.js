5403c2a683ef20d09478fefd378150e2e17ad157
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2025-08-17 20:18:55 -0300

    feat: add mock data for projects display

---
+++ src/components/organisms/projects/projects.js
@@ -0,0 +1,31 @@
+export const projetos = [
+  {
+    id: 1,
+    nome: "Projeto 1",
+    descricao: "Descrição do projeto 1",
+    tecnologias: ["React", "Node.js"],
+    imagem: "https://placehold.co/600x400/EEE/31343C",
+    url: "https://github.com",
+    repositorio: "https://github.com",
+  },
+  {
+    id: 2,
+    nome: "Projeto 2",
+    descricao: "Descrição do projeto 2",
+    tecnologias: ["Vue.js", "Express"],
+    imagem:
+      "https://lncimg.lance.com.br/cdn-cgi/image/width=950,quality=75,fit=pad,format=webp/uploads/2025/08/AGIF25073101062860-aspect-ratio-512-320.jpg",
+    url: "https://github.com",
+    repositorio: "https://github.com",
+  },
+  {
+    id: 3,
+    nome: "Projeto 3",
+    descricao: "Descrição do projeto 3",
+    tecnologias: ["Angular", "Spring"],
+    imagem:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwSdbYhVwRedTbHoE50DV7ASX5a-sIe99k_Q&s",
+    url: "https://github.com",
+    repositorio: "https://github.com",
+  },
+];
