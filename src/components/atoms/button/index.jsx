5b003648d3fe12380ca960fb3210073af1b7713e
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2025-08-24 05:34:35 -0300

    fix: fixing button structure

--- src/components/atoms/button/index.jsx
+++ src/components/atoms/button/index.jsx
@@ -1,8 +1,7 @@
 function Button({ children, onClick }) {
   return (
     <button
-      className="bg-primary text-white py-2 px-4 rounded-lg"
-      style={{ cursor: "pointer" }}
+      className="bg-primary text-white py-2 px-4 rounded-lg cursor-pointer"
       onClick={onClick}
     >
       {children}
