a3a548395d06e9a24c0029dbac3c90e8bed8cf5d
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2025-08-24 05:36:17 -0300

    feat: creating header and footer

---
+++ src/components/organisms/footer/index.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="flex justify-center items-center p-4 text-textSecondary cursor-default">
+      <p className="text-sm">
+        © 2025 Luiz Azevedo. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
