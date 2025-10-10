60d33c517bf9d1904be5cd002122e9a6e0b04b09
luizfelipe0854 <luizfelipedemeloazevedo@gmail.com>
2025-10-09 23:28:16 -0300

    feat: assemble homepage with components

--- src/components/pages/App.jsx
+++ src/components/pages/App.jsx
@@ -1,19 +1,38 @@
 import "./reset.css";
 
-import Button from "../atoms/button";
+import ScrollToTop from "../atoms/scrollToTop";
 
-import IconLink from "../atoms/iconLink";
+import Header from "../organisms/header";
+import Footer from "../organisms/footer";
+import Hero from "../organisms/hero";
 
-import { FaLinkedin } from "react-icons/fa";
+import About from "../organisms/about";
+import ProjectsSection from "../organisms/projects";
+import Skills from "../organisms/skills";
+import WorkExperience from "../organisms/workExperience";
+import Academic from "../organisms/academic";
 
 function App() {
   return (
     <div className="bg-background min-h-screen w-full flex justify-center">
-      <div className="bg-background-light w-[90%] my-6">
-        <Button onClick={() => alert("Clicado!")}>Ver projetos</Button>
-        <IconLink link="https://github.com">
-          <FaLinkedin />
-        </IconLink>
+      <div className="bg-background-light w-[90%] max-w-[1200px] my-6 rounded-lg shadow-lg p-10">
+        <Header />
+
+        <Hero />
+
+        <About />
+
+        <Skills />
+
+        <ProjectsSection />
+
+        <Academic />
+
+        <WorkExperience />
+
+        <ScrollToTop />
+
+        <Footer />
       </div>
     </div>
   );
