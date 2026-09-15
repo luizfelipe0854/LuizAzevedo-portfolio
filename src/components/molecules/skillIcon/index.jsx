eb5f45260ea724102c24a40e41e0ca2cb119cfc2
Luiz Azevedo <luizazevedo.dev@gmail.com>
2026-09-14 21:41:18 -0300

    style: update theme font and cleanup CSS overrides

--- src/components/molecules/skillIcon/index.jsx
+++ src/components/molecules/skillIcon/index.jsx
@@ -1,10 +1,7 @@
 function SkillIcon({ children, label }) {
   return (
     <div className="flex flex-col items-center text-4xl">
-      <div className="p-2 rounded-md duration-200 hover:inset-ring-2 hover:inset-ring-(--color-primary) hover:scale-110 cursor-pointer">
-        {" "}
-        {children}
-      </div>
+      <div className="p-2 rounded-md"> {children}</div>
       <span className="text-sm text-(--color-text-secondary) ">{label}</span>
     </div>
   );
